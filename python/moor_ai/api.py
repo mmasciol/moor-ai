d46e24ff6d51e13c7362cd5a6f7f9038f08b3bed
marco.masciola <marco.masciola@gmail.com>
2023-01-02 13:55:12 -0700

    added sourc definition file

--- python/moor_ai/api.py
+++ python/moor_ai/api.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from ctypes import (
+    c_char_p,
     cdll,
-    c_int
+    c_int,
+    POINTER,
+    Structure
 )
 import os
 import platform
@@ -20,13 +23,22 @@ else:
 lib_name = 'libmoor-ai{}'.format(ext)
 
 
+class Obj(Structure):
+    _fields_ = [('label', POINTER(c_char_p))]
+
+
 class Domain(object):
     try:
         _lib = cdll.LoadLibrary(os.path.join(sys.prefix, lib_name))
     except OSError:
-        _lib = cdll.LoadLibrary(os.path.join('../bin/', lib_name))
+        _lib = cdll.LoadLibrary(os.path.join('./bin/', lib_name))
 
     _lib.api_hook.restype = c_int
+    _lib.api_allocate_domain.restype = c_int
+    _lib.api_free_domain.restype = c_int
+
+    _lib.api_allocate_domain.argtype = [POINTER(Obj)]  # type: ignore
+    _lib.api_free_domain.argtype = [POINTER(Obj)]  # type: ignore
 
     def run(self) -> None:
         size = self._lib.api_hook()
