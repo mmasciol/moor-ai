0fcb8950b9f46f68350e538f8ec204e6557cfcc1
marco.masciola <marco.masciola@gmail.com>
2023-01-07 23:22:50 -0700

    staging

--- src/api.c
+++ src/api.c
@@ -8,6 +8,7 @@
 #include "bstrlib.h"
 #include "def.h"
 #include "sys.h"
+#include "yaml.h"
 
 ERROR_CODE api_allocate_domain(void **d, char **msg)
 {
@@ -51,17 +52,49 @@ ERROR_CODE api_read_yaml_file(void **d, char *fpath, char **msg)
 {
   ERROR_CODE ierr = SAFE;
   bstring message = NULL;
-  void *yaml = NULL;
+  yaml_parser_t parser;
+  yaml_token_t  token;
+  FILE *file = NULL;
 
   RESETERR();
   CHECKERRQ(VERBOSE, __func__);
 
   message = bformat("Reading *.yaml file <%s>", fpath);
-  CHECKERRQ(INFO, message->data);
+  CHECKERRQ(INFO, (const char*)message->data);
   bdestroy(message);
 
-  // yaml = yaml_open_file(fpath);
-  // yaml_close_file(yaml);
+  file = fopen(fpath, "rb");
+  yaml_parser_initialize(&parser);
+  yaml_parser_set_input_file(&parser, file);
+
+do {
+    yaml_parser_scan(&parser, &token);
+    switch(token.type)
+    {
+    /* Stream start/end */
+    case YAML_STREAM_START_TOKEN: puts("STREAM START"); break;
+    case YAML_STREAM_END_TOKEN:   puts("STREAM END");   break;
+    /* Token types (read before actual token) */
+    case YAML_KEY_TOKEN:   printf("(Key token)   "); break;
+    case YAML_VALUE_TOKEN: printf("(Value token) "); break;
+    /* Block delimeters */
+    case YAML_BLOCK_SEQUENCE_START_TOKEN: puts("<b>Start Block (Sequence)</b>"); break;
+    case YAML_BLOCK_ENTRY_TOKEN:          puts("<b>Start Block (Entry)</b>");    break;
+    case YAML_BLOCK_END_TOKEN:            puts("<b>End block</b>");              break;
+    /* Data */
+    case YAML_BLOCK_MAPPING_START_TOKEN:  puts("[Block mapping]");            break;
+    case YAML_SCALAR_TOKEN:  printf("scalar %s \n", token.data.scalar.value); break;
+    /* Others */
+    default:
+      printf("Got token of type %d\n", token.type);
+    }
+    if(token.type != YAML_STREAM_END_TOKEN)
+      yaml_token_delete(&token);
+  } while(token.type != YAML_STREAM_END_TOKEN);
+  yaml_token_delete(&token);
+
+  yaml_parser_delete(&parser);
+  fclose(file);
 
   return ierr;
 CLEAN_UP:
