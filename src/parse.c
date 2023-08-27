44238e1ab4205288beb2291b19d2164ab2067cdf
marco <mmasciol@users.noreply.github.com>
2023-08-26 19:06:37 -0600

    feature/infrastructure (#4)
    * infrastructure added
    
    * paused ci pipeline

--- src/parse.c
+++ src/parse.c
@@ -3,6 +3,7 @@
 #include "bstrlib.h"
 #include "def.h"
 #include "parse.h"
+#include "simclist.h"
 #include "sys.h"
 #include "yaml.h"
 
@@ -29,10 +30,9 @@ ERROR_CODE parse_yaml(Domain *d, char *fpath, char **msg)
     EMITERRQ(FATAL, (const char *)message->data);
   }
 
-  tree.block = NONE;
   tree.label = NULL;
   tree.f = NULL;
-  tree.child = NULL;
+  list_init(&tree.fields);
   tree.nchild = 0;
 
   yaml_parser_set_input_file(&p.parser, fh);
@@ -42,6 +42,7 @@ ERROR_CODE parse_yaml(Domain *d, char *fpath, char **msg)
 
   yaml_parser_delete(&p.parser);
   fclose(fh);
+  list_destroy(&tree.fields);
 
   return ierr;
 CLEAN_UP:
@@ -50,16 +51,17 @@ CLEAN_UP:
   return FATAL;
 }
 
-ERROR_CODE parse_recurse_yaml(YamlTree *this, ParserEvents *p, char **msg)
+ERROR_CODE parse_recurse_yaml(YamlTree *t, ParserEvents *p, char **msg)
 {
   ERROR_CODE ierr = SAFE;
   bstring message = NULL;
-  char current_field[256];
+  char label[256];
+  Field new_field;
+  Field *field = NULL;
 
   EMITERRQ(STACK, __func__);
 
-  current_field[0] = '\0';
-  // if (!current_field[0]) checkpoint();
+  label[0] = '\0';
 
   do {
     if (!yaml_parser_parse(&p->parser, &p->event)) {
@@ -86,26 +88,35 @@ ERROR_CODE parse_recurse_yaml(YamlTree *this, ParserEvents *p, char **msg)
     case YAML_DOCUMENT_END_EVENT:
       puts("<b>End Document</b>");
       break;
-    case YAML_SEQUENCE_START_EVENT:
-      puts("  <<Sequence Start>>");
-      ierr = parse_append_yaml_branch(this, msg);
-      this->child[this->nchild - 1].block = SEQUENCE;
-      ierr = parse_recurse_yaml(&this->child[this->nchild - 1], p, msg);
-      break;
+
     case YAML_SEQUENCE_END_EVENT:
       puts("  <<Sequence End>>");
       return ierr;
       break;
-    case YAML_MAPPING_START_EVENT:
-      puts("  <<Mapping Start>>");
-      ierr = parse_append_yaml_branch(this, msg);
-      // printf("   >>> %ld\n", this->nchild);
-      this->child[this->nchild - 1].block = MAP;
-      ierr = parse_recurse_yaml(&this->child[this->nchild - 1], p, msg);
-      break;
     case YAML_MAPPING_END_EVENT:
       puts("  <<Mapping End>>");
       return ierr;
+      break;
+
+    case YAML_MAPPING_START_EVENT:
+      puts("  <<Mapping Start>>");
+      list_append(&t->fields, &new_field);
+      field = (Field *)list_get_max(&t->fields);
+      checkpoint();
+      field->block = 0;
+      checkpoint();
+      ierr = parse_recurse_yaml(t, p, msg);
+      field = NULL;
+      break;
+    case YAML_SEQUENCE_START_EVENT:
+      puts("  <<Sequence Start>>");
+      list_append(&t->fields, &new_field);
+      field = (Field *)list_get_max(&t->fields);
+      checkpoint();
+      field->block = 0;
+      checkpoint();
+      ierr = parse_recurse_yaml(t, p, msg);
+      field = NULL;
       break;
 
 
@@ -114,15 +125,15 @@ ERROR_CODE parse_recurse_yaml(YamlTree *this, ParserEvents *p, char **msg)
       break;
     case YAML_SCALAR_EVENT:
       // printf("    Got scalar (value %s)\n", p->event.data.scalar.value);
-      if (this->block == MAP) {
-        if (current_field[0] != '\0') {
-          // printf("bloc: [%d]\n", this->block);
-          printf("-----> %s:  %s\n", current_field, p->event.data.scalar.value);
-          current_field[0] = '\0';
-        } else {
-          copy_string(current_field, p->event.data.scalar.value);
-        }
-      }
+      // if (this->block == MAP) {
+      //   if (current_field[0] != '\0') {
+      //     // printf("bloc: [%d]\n", this->block);
+      //     current_field[0] = '\0';
+      //   } else {
+      // copy_string(label, p->event.data.scalar.value);
+      // printf("-----> %s:  %s\n", label, p->event.data.scalar.value);
+      //   }
+      // }
       break;
     }
 
@@ -139,44 +150,25 @@ CLEAN_UP:
   return FATAL;
 }
 
-// ERROR_CODE parse_add_to_bag(YamlTree *t, const char *word, char **msg)
-// {
-//   ERROR_CODE ierr = SAFE;
-//   EMITERRQ(STACK, __func__);
-//
-//   // if (!t->field) {
-//   //   t->field = malloc(sizeof(char *));
-//   // } else {
-//   //   t->field = realloc(t->field, (t->nfield + 1) * sizeof(char *));
-//   // }
-//   // t->nfield += 1;
-//   // t->field[t->nfield - 1] = malloc((strlen(word) + 1) * sizeof(char));
-//   // copy_string(t->field[t->nfield - 1], (unsigned char *)word);
-//
-//   return ierr;
-// CLEAN_UP:
-//   return FATAL;
-// }
-
 ERROR_CODE parse_append_yaml_branch(YamlTree *t, char **msg)
 {
   ERROR_CODE ierr = SAFE;
 
   EMITERRQ(STACK, __func__);
 
-  if (!t->child) {
-    t->child = malloc(1 * sizeof(YamlTree));
-  } else {
-    t->child = realloc(t->child, (t->nchild + 1) * sizeof(YamlTree));
-  }
+  // if (!t->child) {
+  //   t->child = malloc(1 * sizeof(YamlTree));
+  // } else {
+  //   t->child = realloc(t->child, (t->nchild + 1) * sizeof(YamlTree));
+  // }
 
-  t->nchild++;
+  // t->nchild++;
 
-  t->child[t->nchild - 1].block = NONE;
-  t->child[t->nchild - 1].label = NULL;
-  t->child[t->nchild - 1].f = NULL;
-  t->child[t->nchild - 1].child = NULL;
-  t->child[t->nchild - 1].nchild = 0;
+  // t->child[t->nchild - 1].block = NONE;
+  // t->child[t->nchild - 1].label = NULL;
+  // t->child[t->nchild - 1].f = NULL;
+  // t->child[t->nchild - 1].child = NULL;
+  // t->child[t->nchild - 1].nchild = 0;
 
   return ierr;
 CLEAN_UP:
@@ -189,12 +181,17 @@ ERROR_CODE parse_free_yaml_tree(YamlTree *c, char **msg)
 
   EMITERRQ(STACK, __func__);
 
-  if (c->child) {
-    ierr = parse_free_yaml_tree(c->child, msg);
-    free(c->child);
-  }
+  // if (c->child) {
+  //   ierr = parse_free_yaml_tree(c->child, msg);
+  //   free(c->child);
+  // }
 
   return ierr;
 CLEAN_UP:
   return FATAL;
+}
+
+size_t _list_field_meter(const void *el)
+{
+  return sizeof(Field);
 }
