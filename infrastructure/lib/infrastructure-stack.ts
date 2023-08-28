90e3085bb7301eaf0e35e0d5f84f8917c0c0129d
marco.masciola <marco.masciola@gmail.com>
2023-08-27 22:50:27 -0600

    trying again

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -10,8 +10,7 @@ export class InfrastructureStack extends cdk.Stack {
       pipelineName: 'PipelineMoorAI',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('mmasciol/moor-ai', 'maint/infrastructure'),
-        commands: ['cd infrastructure', 'npm ci', 'npm run build', 'npx cdk synth'],
-        primaryOutputDirectory: "infrastructure/cdk.out"
+        commands: ['cd infrastructure', 'npm ci', 'npm run build', 'npx cdk synth']
       })
     });
   }
